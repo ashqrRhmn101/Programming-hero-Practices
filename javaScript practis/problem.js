cf76d09873d768e8379d97bb3c717b7da4ce34cf
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-27 17:40:29 +0600

    For loop Add

--- javaScript practis/problem.js
+++ javaScript practis/problem.js
@@ -3,11 +3,45 @@
 
 // console.log("remainder =", remainder);
 
-//  Even & Odd
-const num = 10;
+        //  Even & Odd
+// const num = 10;
 
-if (num % 2 == 0) {
-  console.log("Even");
-} else {
-  console.log("Odd");
+// if (num % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Odd");
+// }
+
+        // for loop
+
+// for (var x = 1; x <= 10; x++) {
+//   // document.write("<h2>Hello</h2>");
+//   document.write(" ", x);
+// }
+
+
+        // Odd number
+for (var x = 1; x <= 20; x= x + 2) {
+  document.write(" ", x);
 }
+        // even number
+
+for(var x = 2; x<=20; x = x+2)
+{
+    document.write(x," <br>");
+}
+// negative
+for(var x = 30; x>=2; x = x-2)
+{
+    document.write(" ");
+    document.write(x);
+}
+
+// 1 to 10 = (total)
+
+var sum=0, x;
+for(x=1; x<=5; x++)
+{
+    sum += x;
+}
+ document.write("<br> Total = > \n", sum); 
