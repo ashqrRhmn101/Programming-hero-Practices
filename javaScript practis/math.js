6d19aa21c8aedda11c029560a40f1865c21546a1
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-27 12:53:30 +0600

    javaScript Practice Problem 1

---
+++ javaScript practis/math.js
@@ -0,0 +1,22 @@
+// var sum = 10.210;
+// sum += 20;
+
+// var taka = 0.1;
+// var dolor = parseFloat("0.2");
+
+// var total = taka+dolor;
+
+// console.log (total.toFixed(4));
+
+        //  practice Problem 1
+var totalMoney = 1000;
+
+var orangePrice = 300;
+var applesPrice = 400;
+
+var sum = (orangePrice+applesPrice);
+
+returns = (totalMoney-sum);
+
+console.log(returns);
+
