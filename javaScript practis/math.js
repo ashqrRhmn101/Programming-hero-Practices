e0094a3f8c258fad37c0b5e94328fbf79d2aca45
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-27 13:18:55 +0600

    add let veriable & problem 2 solved

--- javaScript practis/math.js
+++ javaScript practis/math.js
@@ -8,15 +8,39 @@
 
 // console.log (total.toFixed(4));
 
-        //  practice Problem 1
+// let money = 100;    // প্রথমে 100 টাকা
+// console.log("শুরুতে টাকা:", money);
+
+// money = money - 40; // 40 টাকা খরচ করলাম
+// console.log("৪০ টাকা খরচের পরে:", money);
+
+// money = money + 20; // আবার 20 টাকা পেলাম
+// console.log("২০ টাকা পাওয়ার পরে:", money);
+
+//  practice Problem 1
 var totalMoney = 1000;
 
 var orangePrice = 300;
 var applesPrice = 400;
 
-var sum = (orangePrice+applesPrice);
+var sum = orangePrice + applesPrice;
 
-returns = (totalMoney-sum);
+var returns = totalMoney - sum;
 
 console.log(returns);
 
+//  practice Problem 2
+
+// const math = 75.25, biology = 65, chemistry = 80, physics = 35.45, bangla = 99.50;
+
+const math = 75.25;
+const biology = 65;
+const chemistry = 80;
+const physics = 35.45;
+const bangla = 99.5;
+
+const total = math + biology + chemistry + physics + bangla;
+
+const avg = total / 5;
+
+console.log("Total Subject Average : ", avg.toFixed(2));
