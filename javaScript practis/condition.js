ee8b6ae38f25bb490e6e5804749a4ca5eb4d0dae
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-28 12:20:43 +0600

    BMI problem solve

--- javaScript practis/condition.js
+++ javaScript practis/condition.js
@@ -13,3 +13,35 @@ if(burger>500)
 else{
     console.log("Drinks Price 30TK")
 }
+
+/*** 
+
+BMI Calculator and Health Category
+
+Create a JavaScript program that calculates the Body Mass Index (BMI) and assigns a health category based on the BMI value. Use nested if-else statements to determine the health category.
+
+    - Calculate BMI using the formula: BMI = weight (kg) / (height (m))^2
+    - BMI < 18.5, you are underweight.
+    - BMI >= 18.5 and BMI <=24.9, you are normal.
+    - BMI >=25 and BMI <= 29.9, you are overweight.
+    - Otherwise, you are obese.
+
+*/
+
+const BMI=24.9;
+ if(BMI<18.5)
+ {
+    console.log("You are underweight")
+ }
+ else if(BMI>=18.5 && BMI<=24.9)
+ {
+    console.log("You are Normal")
+ }
+ else if(BMI>=25 && BMI<=29.9)
+ {
+    console.log("You are Overweight")
+ }
+
+ else{
+    console.log("You are obese")
+ }
