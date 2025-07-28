fe03dd2c920b16c0950bd478bac0254f3524705e
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-28 15:42:15 +0600

    num1&num2 one line solve if else for javaScript

--- javaScript practis/condition.js
+++ javaScript practis/condition.js
@@ -95,3 +95,34 @@ if (myScore > 80) {
 } else {
   console.log("go to home and sleep and act sad");
 }
+
+
+/***
+
+you have two numbers in two variables, called: num1, num2
+
+now declare a variable called result. 
+if num1 is bigger than num2 then result will be double of num1. if not, then the value of the variable result will be the sum of num1 and num2.
+
+write a simple if-else. 
+
+also, write it using ternary operator.
+
+ */
+
+let num1 = 21;
+let num2 = 20;
+
+
+let result= (num1>num2) ? (num1*2) : (num1+num2);
+
+// if(num1>num2)
+// {
+//     result = num1*2;
+// }
+// else
+// {
+//     result= num1+num2;
+// }
+
+console.log("Result = ", result);
