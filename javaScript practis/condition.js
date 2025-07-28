c0fd22523ed4ecbde43de31e4211d2f774cc6a1f
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-28 12:12:02 +0600

    if, else add & problem solve 1

---
+++ javaScript practis/condition.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+let burger = 500;
+
+if(burger>500)
+{
+    console.log("Free Drinks")
+}
+else{
+    console.log("Drinks Price 30TK")
+}
