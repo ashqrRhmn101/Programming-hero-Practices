9144f0dfe4606991b5d490002999508183c5a858
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-07-28 15:22:43 +0600

    myScore and myScore add

--- javaScript practis/condition.js
+++ javaScript practis/condition.js
@@ -64,3 +64,34 @@ if (mark >= 90 && mark <= 100) {
 } else {
   console.log("F");
 }
+
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+
+let myScore = 75;
+let myFriendScore = 80;
+
+if (myScore > 80) {
+  if (myFriendScore >= 80) {
+    console.log("Go for a lunch");
+  }
+  if (myFriendScore >= 60) {
+    console.log("Good luck");
+  } else if (myFriendScore >= 40) {
+    console.log("My friend's message unseen");
+  } else {
+    console.log("block your friend");
+  }
+} else {
+  console.log("go to home and sleep and act sad");
+}
