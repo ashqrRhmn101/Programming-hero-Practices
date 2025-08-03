b5bc9184d57a0b516df64dcaa1dc4cd2c280fe0a
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-03 15:04:54 +0600

    Task - 3 complete

--- javaScript practis/stringTask.js
+++ javaScript practis/stringTask.js
@@ -24,6 +24,35 @@ for (let i = 0; i < word.length; i++) {
 }
 console.log(count);
 
-/*    */
+/*  Task-3:
+Check whether a string contains all the vowels a, e, i, o, u  */
+// vowel Count;
+const vowel = "apple An orange a pen";
+count = 0;
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+for(let j = 0; j < vowel.length; j++)
+{
+    if(vowels.includes(vowel[j].toLowerCase()))
+    {
+        count++;
+    }
+}
+console.log(count);
+
+// Check whether
+let a = vowel.includes("a");
+let e = vowel.includes("e");
+let i = vowel.includes("i");
+let o = vowel.includes("o");
+let u = vowel.includes("u");
+
+if (a && e && i && o && u) {
+  console.log("All vowels are present.");
+} else {
+  console.log("All vowels are NOT present.");
+}
+
+
 
 /*    */
