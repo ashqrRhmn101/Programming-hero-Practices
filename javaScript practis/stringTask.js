5d228f39e0471a0ec4d8a323b36dbfe967b83d73
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-03 12:55:27 +0600

    Task 1 complete

---
+++ javaScript practis/stringTask.js
@@ -0,0 +1,21 @@
+/*   Task-1:
+ Count how many times a string has the letter a */
+
+const text = "I am a student and programmer";
+let count = 0;
+
+for (let x = 0; x < text.length; x++) {
+  if (text[x] === "a") {
+    count++;
+  }
+}
+console.log(count);
+
+/*    */
+
+
+
+/*    */
+
+
+/*    */
