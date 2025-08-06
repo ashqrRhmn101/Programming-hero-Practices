4e8efae05960d7cf2ccd9769694b0a4dfc559685
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-06 16:44:49 +0600

    task7 problem solve

---
+++ array-looping-tasks-practice/task7.js
@@ -0,0 +1,28 @@
+/*
+Task 7
+Given a 2D array, update the value at second row first item to 99 and print the updated array.
+
+input:
+[
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]
+Expected Array:
+[
+  [1, 2],
+  [99, 4],
+  [5, 6]
+]
+  */
+
+let arrays = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+];
+let array = arrays;
+// array[1] = [99, 4];
+array[1][0] = 99;
+
+console.log(array);
