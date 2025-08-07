6fdac717b266e592b50428a1199ebb8d714a2c64
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-07 16:08:18 +0600

    simple function

---
+++ javaScript practis/function.js
@@ -0,0 +1,8 @@
+let hed = document.getElementById("myHeading");
+hed.innerText = "Hello Java";
+
+function myFunction(){
+    let hed = document.getElementById("myHeading");
+    hed.style.color = "red";
+    hed.innerText = "Hello JavaScript";
+}
