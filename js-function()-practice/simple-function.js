dd4204692f0749d7b3d62880bb0dfbbd04df588c
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-08-08 16:48:17 +0600

    use return function and some math problem solve

--- js-function()-practice/simple-function.js
+++ js-function()-practice/simple-function.js
@@ -1,5 +1,26 @@
-function sum(x,y){
-    const result = x+y;
-    console.log(result);
+function sum(x, y) {
+  const result = x + y;
+  console.log(result);
 }
-sum(20,30);
+sum(20, 30);
+
+// return value add
+
+function sum2(a, b) {
+  const total = a + b;
+  return total;
+}
+const totals = sum2(10, 20);
+console.log(totals);
+
+// simple Square math
+
+function square(number){
+    console.log("Value Number ", number);
+    const ttl = number * number ;
+
+    return ttl;
+    // console.log("Square value ", ttl);
+}
+const res = square(5);
+console.log("Square value ", res);
